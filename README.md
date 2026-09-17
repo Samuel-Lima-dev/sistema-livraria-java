53cdb62a834120445eb10271cc959efb3076e193
Samuel-Lima-Dev <samueldelima340@gmail.com>
2026-09-16 22:58:16 -0300

    feat: estrutura inicial do projeto com classe abstrata Produto

---
+++ README.md
@@ -0,0 +1,9 @@
+# 📚 Sistema de Gerenciamento de Livraria
+
+Projeto desenvolvido durante as aulas para praticar conceitos fundamentais de **Programação Orientada a Objetos (POO)** em Java.
+
+## 🚀 Tecnologias e Conceitos Aplicados
+- **Linguagem:** Java
+- **POO:** Abstração, Encapsulamento, Herança e Polimorfismo
+
+
---
+++ src/livraria/Produto.java
@@ -0,0 +1,43 @@
+package livraria;
+
+public class Produto {
+	private String nome;
+	private double preco;
+	private String isbn;
+	
+	//Construtor
+	public Produto(String nome, double preco, String isnb){
+		setNome(nome);
+		setPreco(preco);
+		setIsbn(isbn);
+	}
+	
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getPreco() {
+		return this.preco;
+	}
+	
+	public void setPreco(double valor) {
+		if(valor > 0) {
+			this.preco = valor;
+		}
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	
+}
