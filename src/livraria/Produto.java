5857acd9eecf6addf2381c5caad6aed5f78642e2
Samuel-Lima-Dev <samueldelima340@gmail.com>
2026-09-24 16:34:51 -0300

    Adicionando validações no metodo setQuantidade e setNome

--- src/livraria/Produto.java
+++ src/livraria/Produto.java
@@ -3,39 +3,53 @@ package livraria;
 public abstract class Produto {
 	private String nome;
 	private double preco;
-	private String isbn;
 	private int quantidadeEstoque;
 	
-	//Construtor
-	public Produto(String nome, double preco, String isnb){
+	// Construtores
+	public Produto(String nome, double preco) {
 		setNome(nome);
 		setPreco(preco);
-		setIsbn(isbn);
+		this.quantidadeEstoque = 0;
 	}
 	
+	public Produto(String nome, double preco, int quantidadeEstoque) {
+		setNome(nome);
+		setPreco(preco);
+		setQuantidade(quantidadeEstoque);
+	}
+	
+	// Método abstrato
 	public abstract void exibirDetalhes();
+
 	
 	public void adicionarEstoque(int quantidade) {
-		
-		if(quantidade > 0) {
-			this.setQuantidade(getQuantidade()+quantidade);
+		if (quantidade > 0) {
+			setQuantidade(getQuantidade() + quantidade);
+		} else {
+			System.out.println("Quantidade para adicionar deve ser maior que zero.");
 		}
 	}
 	
 	public boolean darBaixaEstoque(int quantidade) {
-		if(quantidade > 0 && this.quantidadeEstoque >= quantidade) {
-			setQuantidade(getQuantidade()-quantidade);
+		if (quantidade > 0 && this.quantidadeEstoque >= quantidade) {
+			setQuantidade(getQuantidade() - quantidade);
 			return true;
 		}
+		System.out.println("Estoque insuficiente ou quantidade inválida.");
 		return false;
 	}
 	
+	// Getters e Setters 
 	public String getNome() {
 		return this.nome;
 	}
 	
 	public void setNome(String nome) {
-		this.nome = nome;
+		if(nome != null && !nome.trim().isEmpty()) {
+			this.nome = nome;
+		} else {
+			this.nome = "Produto Sem Nome";
+		}
 	}
 	
 	public double getPreco() {
@@ -43,15 +57,11 @@ public abstract class Produto {
 	}
 	
 	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-	
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
+		if (preco >= 0) {
+			this.preco = preco;
+		} else {
+			System.out.println("O preço não pode ser negativo.");
+		}
 	}
 	
 	public int getQuantidade() {
@@ -59,9 +69,10 @@ public abstract class Produto {
 	}
 	
 	public void setQuantidade(int quantidade) {
-		if(quantidade > 0) {
+		if (quantidade >= 0) {
 			this.quantidadeEstoque = quantidade;
+		} else {
+			System.out.println("A quantidade em estoque não pode ser negativa.");
 		}
 	}
-	
 }
